3361dff4d6a263de9b2a23a173d68a821edf29d1
tushar-2811 <tusharrawat52@gmail.com>
2024-09-03 12:59:11 +0530

    add logoTicker

---
+++ src/components/Logoticker.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Logoticker = () => {
+  return (
+    <div className='bg-black text-white'>
+        <div className='container'>
+           <h2>Trusted by the world's most innovative teams</h2>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Logoticker
